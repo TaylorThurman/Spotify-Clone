b3f90f37713314dee7aef986fb96b7b1074401de
Taylor Thurman <taylor.john.thurman@outlook.com>
2021-03-22 11:32:06 -0500

    finished tutorial

--- app/src/main/java/com/plcoding/spotifycloneyt/di/AppModule.kt
+++ app/src/main/java/com/plcoding/spotifycloneyt/di/AppModule.kt
@@ -5,6 +5,8 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.plcoding.spotifycloneyt.R
+import com.plcoding.spotifycloneyt.adapters.SwipeSongAdapter
+import com.plcoding.spotifycloneyt.exoplayer.MusicServiceConnection
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,6 +17,16 @@ import javax.inject.Singleton
 @Module
 @InstallIn(ApplicationComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideMusicServiceConnection(
+        @ApplicationContext context: Context
+    ) = MusicServiceConnection(context)
+
+    @Singleton
+    @Provides
+    fun providesSwipeSongAdapter() = SwipeSongAdapter()
 
     @Singleton
     @Provides
